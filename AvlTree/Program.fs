2d54193647d9452335fdc23f0ae9f20581d80565
Daniil Kosachev <kos-dan@mail.ru>
2019-08-17 18:36:35 +0300

    Add function 'testAdd'

--- AvlTree/Program.fs
+++ AvlTree/Program.fs
@@ -3,8 +3,11 @@ let random = System.Random()
 let createTreeWithItems =
     Seq.fold AvlTree.add AvlTree.createEmpty
 
-let createRandomItemsTree itemCount maxItem =
+let getRandomSeq itemCount maxItem =
     Seq.init itemCount (fun _ -> random.Next(maxItem))
+
+let createRandomItemsTree itemCount maxItem =
+    getRandomSeq itemCount maxItem
     |> createTreeWithItems
 
 let removeRandom tree maxItem =
@@ -14,12 +17,13 @@ let timeRemove minCount maxCount step  =
     let executionTimeMsec func =
         let timer = System.Diagnostics.Stopwatch()
         timer.Start()
-        func() |> ignore
+        let temp = func()
         timer.ElapsedMilliseconds
     let timeRandomItemRemoving tree maxItem =
         executionTimeMsec (fun () -> removeRandom tree maxItem)
     seq {for c in minCount .. step .. (maxCount + 1) -> (c * 10, c) }
-    |> Seq.map (
+    |> Seq.toList
+    |> List.map (
         (fun (maxItem, count) -> (maxItem, (createRandomItemsTree count maxItem))) >>
         (fun (maxItem, tree) -> timeRandomItemRemoving tree maxItem)
         )
@@ -27,9 +31,17 @@ let timeRemove minCount maxCount step  =
 let writeToFile filename =
     Seq.cast<string> >> (fun items -> System.IO.File.AppendAllLines(filename, items))
 
+let testAdd() =
+    let items = getRandomSeq 100 1000 |> Seq.toList
+    let tree = createTreeWithItems items
+    let good = items |> List.forall (AvlTree.contains tree)
+    System.Diagnostics.Debug.Assert(good, "Tree is not good")
 
 [<EntryPoint>]
 let main argv =
-    let times = timeRemove 100 10000 100
-    writeToFile "times.txt" times
+    testAdd()
+    let times = timeRemove 1000 10000 1000 |> Seq.toList
+    times
+    |> Seq.iter (fun x -> printfn "%d" x)
+    // writeToFile "times.txt" times
     0 // return an integer exit code
