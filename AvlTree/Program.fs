d50be644d4aa7a9c5ffaa9b13acfccb309fe6bd8
Daniil Kosachev <kos-dan@mail.ru>
2019-08-17 17:03:00 +0300

    function 'removeRandom', 'timeRemove'

--- AvlTree/Program.fs
+++ AvlTree/Program.fs
@@ -1,13 +1,33 @@
+let random = System.Random()
+
 let createTreeWithItems =
     Seq.fold AvlTree.add AvlTree.createEmpty
 
 let createRandomItemsTree itemCount maxItem =
-    let random = System.Random()
     Seq.init itemCount (fun _ -> random.Next(maxItem))
     |> createTreeWithItems
+
+let removeRandom tree maxItem =
+    AvlTree.remove tree (random.Next(maxItem))
+
+let timeRemove minCount maxCount step  =
+    let executionTimeMsec func =
+        let timer = System.Diagnostics.Stopwatch()
+        timer.Start()
+        func() |> ignore
+        timer.ElapsedMilliseconds
+    let timeRandomItemRemoving tree maxItem =
+        executionTimeMsec (fun () -> removeRandom tree maxItem)
+    seq {minCount .. step .. (maxCount + 1)}
+    |>
+    Seq.map (
+        (fun x -> (10 * x, createRandomItemsTree x (10 * x))) >>
+        (fun (maxItem, tree) -> timeRandomItemRemoving tree maxItem)
+        )
 
 
 [<EntryPoint>]
 let main argv =
+
     printfn "%A" argv
     0 // return an integer exit code
